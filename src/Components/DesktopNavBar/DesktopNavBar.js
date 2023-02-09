e0a9be548c11f4e5b0114caf0984e0c962e3ceec
Pass80 <71611559+Pass80@users.noreply.github.com>
2023-02-10 00:19:46 +0100

    adding the DesktopNavBar component

---
+++ src/Components/DesktopNavBar/DesktopNavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./DesktopNavBar.css";
+
+const DesktopNavBar = () => {
+	return (
+		<div className="desktop-nav">
+			<ul>
+				<li>Home</li>
+				<li>Search</li>
+				<li>Favorites</li>
+				<li>Account</li>
+			</ul>
+		</div>
+	);
+};
+
+export default DesktopNavBar;
--- src/Pages/AreasPage/AreasPage.js
+++ src/Pages/AreasPage/AreasPage.js
@@ -13,6 +13,7 @@ import { Link } from "react-router-dom";
 import WideCard from "../../Components/WideCard/WideCard";
 import SmallButton from "../../Components/SmallButton/SmallButton";
 import Button from "../../Components/Button/Button";
+import DesktopNavBar from "../../Components/DesktopNavBar/DesktopNavBar";
 
 const AreasPage = () => {
 	const [selectedArea, setSelectedArea] = useState();
@@ -42,50 +43,69 @@ const AreasPage = () => {
 
 	return (
 		<div className="category-page">
-			<header>
-				<div className="header-container">
-					<Link className="back-arrow-link" to={"/main"}>
-						<HiArrowNarrowLeft className="back_arrow" />
-					</Link>
-					<h2>Areas</h2>
-				</div>
-			</header>
-			<SearchBar setSearchResults={setSearchResults} />
-			{searchResults && (
-				<section className="search-results">
-					{/* Wenn der searchResults-Wert nicht null ist, werden die Karten 
-            mit den Suchergebnissen gezeigt. */}
-					{searchResults &&
-						searchResults.map((meal) => (
-							<WideCard key={meal.idMeal} meal={meal} />
-						))}
-				</section>
-			)}
-			<div className="wide-screen">
-				{mealSort?.map((element) => {
-					return (
-						<Button button={element}>{element.strCategory}</Button>
-					);
-				})}
-			</div>
-			{selectedArea && !searchResults && (
-				<section className="areas-section">
-					<div className="title sm-btn">
-						<h3>Categories</h3>
-						{/* <p className="seeAll">See All</p> */}
+			<div className="fixed-section">
+				<header>
+					<div className="header-container">
+						<Link className="back-arrow-link" to={"/main"}>
+							<HiArrowNarrowLeft className="back_arrow" />
+						</Link>
+
+						<h2>Areas</h2>
+						<DesktopNavBar />
+					</div>
+				</header>
+				<SearchBar
+					className="search-bar"
+					setSearchResults={setSearchResults}
+				/>
+				<div className="wide-screen">
+					<div className="titles-heading">
+						<h2>Categories</h2>
 						<SmallButton />
 					</div>
-					<Carousel data={mealSort} button={true} type2={true} />
-				</section>
-			)}
 
-			{selectedArea && !searchResults && (
-				<section className="results-section">
-					{selectedArea?.map((meal, index) => (
-						<SmallCard meal={meal} key={index} />
-					))}
-				</section>
-			)}
+					<div className="buttons-container">
+						{mealSort?.map((element) => {
+							return (
+								<Button button={element}>
+									{element.strCategory}
+								</Button>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+			<div className="display-section">
+				{searchResults && (
+					<section className="search-results">
+						{/* Wenn der searchResults-Wert nicht null ist, werden die Karten 
+                         mit den Suchergebnissen gezeigt. */}
+						{searchResults &&
+							searchResults.map((meal) => (
+								<WideCard key={meal.idMeal} meal={meal} />
+							))}
+					</section>
+				)}
+
+				{selectedArea && !searchResults && (
+					<section className="areas-section">
+						<div className="title sm-btn">
+							<h3>Categories</h3>
+							{/* <p className="seeAll">See All</p> */}
+							<SmallButton />
+						</div>
+						<Carousel data={mealSort} button={true} type2={true} />
+					</section>
+				)}
+
+				{selectedArea && !searchResults && (
+					<section className="results-section">
+						{selectedArea?.map((meal, index) => (
+							<SmallCard meal={meal} key={index} />
+						))}
+					</section>
+				)}
+			</div>
 
 			<NavBar />
 		</div>
