d6bcc7069a37a2e1c562c86cdebf2a53d9d4d15b
Pass80 <71611559+Pass80@users.noreply.github.com>
2023-02-09 18:20:44 +0100

    adding media query to the AreasPage and Button component

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
 
 const Button = ({ button }) => {
-    return (
-        <>
-             {/* Dies wird das Komponente bedingt gemäß den Daten, die es erhält,
+	return (
+		<>
+			{/* Dies wird das Komponente bedingt gemäß den Daten, die es erhält,
             rendern. Entweder areas Buttons wird gezeigt oder Categories buttons */}
-            {button.strArea && (
-                <Link className="btn-link" to={`/area/${button.strArea}`}>
-                    <div className="element-btn">{button.strArea}</div>
-                </Link>
-            )}
-            {button.strCategory && (
-                <Link
-                    className="btn-link"
-                    to={`/category/${button.strCategory}`}
-                >
-                    <div className="element-btn">{button.strCategory}</div>
-                </Link>
-            )}
-        </>
-    );
+			{button.strArea && (
+				<button className="element-btn">
+					<Link className="btn-link" to={`/area/${button.strArea}`}>
+						{button.strArea}
+					</Link>
+				</button>
+			)}
+			{button.strCategory && (
+				<button className="element-btn">
+					<Link
+						className="btn-link"
+						to={`/category/${button.strCategory}`}
+					>
+						{button.strCategory}
+					</Link>
+				</button>
+			)}
+		</>
+	);
 };
 
 export default Button;
